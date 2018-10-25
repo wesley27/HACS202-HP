953ca34ac56940ba304280f3c6719e9686b1564d
ctbyrd <ctbyrd9@gmail.com>
2018-10-25 17:18:07 -0400

    Update Data_Analytics.py
    Added a function to compute a scatter plot with a regression line

--- Data_Analytics.py
+++ Data_Analytics.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot
 data = pd.read_csv("/Users/CAM_CAM_1/Downloads/????")
 #print(data.columns)
 
-def function1():
+def histogram():
     """
     The python functions below will work to create a histogram.
     """
@@ -19,13 +19,29 @@ def function1():
     plt.ylabel("")
     plt.show()
 
-def function3():
+def scatter():
     """
     The python functions below will work to create a scatter plot.
     """
     plt.style.use('seaborn')
     fig = plt.figure()
     plt.scatter(data[""], data[""], marker='.')
+    plt.title("")
+    plt.xlabel("")
+    plt.ylabel("")
+    plt.show()
+
+def scatter_regression():
+    """
+    The python functions below will work to create a scatter plot with a
+    regresison line.
+    """
+    plt.style.use('seaborn')
+    fig = plt.figure()
+    plt.scatter(data[""], data[""], marker='.')
+    m, b = np.polyfit(data[""], data[""], 1)
+    x = np.linspace(10,1000, 10000)
+    plt.plot(x,m*x+b, linestyle='--')
     plt.title("")
     plt.xlabel("")
     plt.ylabel("")

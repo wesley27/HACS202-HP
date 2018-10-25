454dd91cb26f6c523f3310bd841543035da54180
ctbyrd <ctbyrd9@gmail.com>
2018-10-25 17:15:24 -0400

    Update Data_Analytics.py
    Added a funciton for making a scatter plot visualization.

--- Data_Analytics.py
+++ Data_Analytics.py
@@ -4,19 +4,29 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot
 
-data = pd.read_csv("/Users/CAM_CAM_1/Downloads/comicmovies.csv")
+data = pd.read_csv("/Users/CAM_CAM_1/Downloads/????")
 #print(data.columns)
 
 def function1():
     """
-    The python functions below will work to create a histogram of the Review
-    column in the day.csv data set that was imported.
+    The python functions below will work to create a histogram.
     """
     plt.style.use('seaborn')
     fig = plt.figure()
-    plt.hist(data["Review"])
-    plt.title("Comic Movie Review Distribution")
-    plt.xlabel("Rating of Reviews")
-    plt.ylabel("Number of Reviews")
+    plt.hist(data[""])
+    plt.title("")
+    plt.xlabel("")
+    plt.ylabel("")
     plt.show()
-    
+
+def function3():
+    """
+    The python functions below will work to create a scatter plot.
+    """
+    plt.style.use('seaborn')
+    fig = plt.figure()
+    plt.scatter(data[""], data[""], marker='.')
+    plt.title("")
+    plt.xlabel("")
+    plt.ylabel("")
+    plt.show()

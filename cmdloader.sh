2feea815a666fee05b9ca37d6b3edca06fd95c2d
wesley27 <wesley27@users.noreply.github.com>
2018-10-27 20:57:05 -0400

    add cmdloader for script usage

---
+++ cmdloader.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# This script loads necessary commands into the user
+# script directory for programmatic usage as system commands.
+
+# Enabling scripts to be called as system commands eliminates
+# the need for loading them as imports.
+cp mitm-ssr /usr/local/bin/
+cp honeypot.py /usr/local/bin/
+cp hp-recycler /usr/local/bin/

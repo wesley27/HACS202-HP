52f212524e1ed223f94a9693c0d1727ee854a0fb
wesley27 <wesley27@users.noreply.github.com>
2018-12-06 15:14:09 -0500

    almost finish session extraction

--- dextract-sessioninfo.py
+++ dextract-sessioninfo.py
@@ -14,9 +14,10 @@ Usage: ./dextract-sessioninfo.py
 CSV_NAME = 'session_data.csv'
 
 def extract_session_data():
-    csv = open(CSV_NAME, 'wb+')
+    csv = open(CSV_NAME, 'ab+')
     filewrite = csv.write(csv, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    filewriter.writerow([]) # column headers
+    if not os.path.isfile(CSV_NAME) or os.path.getsize(CSV_NAME) <= 0:
+        filewriter.writerow(['timestamp', 's_id', 'c_id', 'atk_ip']) # column headers
 
     session_pattern = re.compile('^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}')
 
@@ -25,8 +26,37 @@ def extract_session_data():
             continue
 
         session_file = open(filename)
+        timestamp = ''
+        s_id = ''
+        c_id = ''
+        atk_ip = ''
+        atk_cmd = []
+        atk_output = []
+        at_output = False
         for line in session_file:
             #process file line by line
+            if 'Date' in line:
+                dat = line.split(': ')
+                timestamp = dat[1][:-1]
+            elif 'Session ID' in line:
+                dat = line.split(': ')
+                s_id = dat[1][:-1]
+            elif 'Container ID:' in line:
+                dat = line.split(': ')
+                c_id = dat[1][:-1]
+            elif 'Attacker IP' in line:
+                dat = line.split(': ')
+                atk_ip = dat[1][:-1]
+            elif 'command' in line:
+                dat = line.split(': ')
+                atk_cmd.append(dat[1][:-1])
+            elif 'Output Below' in line:
+                at_output = True
+                continue
+            elif at_output:
+                atk_output.append(line[:-1])
+            elif filename in line:
+                at_output = False
 
         session_file.close()
 
